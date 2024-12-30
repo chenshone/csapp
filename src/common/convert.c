56a36d62d8ba79a2984dae27fca20ad90a31b996
chenshone <1351766353@qq.com>
2024-12-30 22:22:27 +0800

    impl str2uint

--- src/common/convert.c
+++ src/common/convert.c
@@ -10,7 +10,113 @@ uint64_t string2uint(const char *str) {
 }
 
 uint64_t string2uint_range(const char *str, int start, int end) {
-    return 0;
+    // start: starting index inclusive
+    // end: ending index exclusive
+
+    end = (end == -1) ? strlen(str) - 1 : end;
+    uint64_t uv = 0;
+    int sign_bit = 0; // 0 for positive, 1 for negative
+
+    // DFA: deterministic finite automata to scan string and get value
+    int state = 0;
+
+    for (int i = start; i <= end; ++i) {
+        const char c = str[i];
+
+        if (state == 0) {
+            if (c == '0') {
+                state = 1;
+                uv = 0;
+            } else if (c >= '1' && c <= '9') {
+                state = 2;
+                uv = c - '0';
+            } else if (c == '-') {
+                state = 3;
+                sign_bit = 1;
+            } else if (c == ' ')
+                state = 0;
+            else { goto fail; }
+        } else if (state == 1) {
+            if (c >= '0' && c <= '9') {
+                state = 2;
+                uv = uv * 10 + (c - '0');
+            } else if (c == 'x')
+                state = 4;
+            else if (c == ' ')
+                state = 6;
+            else { goto fail; }
+        } else if (state == 2) {
+            if (c >= '0' && c <= '9') {
+                state = 2;
+                uint64_t pv = uv;
+                uv = uv * 10 + c - '0';
+                // maybe overflow
+                if (pv > uv) {
+                    printf("(uint64_t)%s overflow: cannot convert\n", str);
+                    goto fail;
+                }
+            } else if (c == ' ') {
+                state = 6;
+            } else { goto fail; }
+        } else if (state == 3) {
+            if (c == '0') {
+                state = 1;
+            } else if (c >= '1' && c <= '9') {
+                state = 2;
+                uv = c - '0';
+            } else { goto fail; }
+        } else if (state == 4) {
+            if (c >= '0' && c <= '9') {
+                state = 5;
+                uv = uv * 16 + c - '0';
+            } else if ('a' <= c && c <= 'f') {
+                state = 5;
+                uv = uv * 16 + c - 'a' + 10;
+            } else { goto fail; }
+        } else if (state == 5) {
+            // hex
+            if (c >= '0' && c <= '9') {
+                state = 5;
+                uint64_t pv = uv;
+                uv = uv * 16 + c - '0';
+                // maybe overflow
+                if (pv > uv) {
+                    printf("(uint64_t)%s overflow: cannot convert\n", str);
+                    goto fail;
+                }
+            } else if (c >= 'a' && c <= 'f') {
+                state = 5;
+                uint64_t pv = uv;
+                uv = uv * 16 + c - 'a' + 10;
+                // maybe overflow
+                if (pv > uv) {
+                    printf("(uint64_t)%s overflow: cannot convert\n", str);
+                    goto fail;
+                }
+            } else { goto fail; }
+        } else if (state == 6) {
+            if (c == ' ') {
+                state = 6;
+            } else { goto fail; }
+        }
+    }
+
+    if (sign_bit == 0) {
+        return uv;
+    }
+
+    // sign_bit == 1
+    if ((uv & 0x8000000000000000) != 0) {
+        printf("(int64_t)%s: signed overflow: cannot convert\n", str);
+        exit(0);
+    }
+    int64_t sv = -1 * (int64_t) uv;
+    return *((uint64_t *) &sv);
+
+
+fail:
+    printf("type converter: <%s> cannot be converted to integer\n", str);
+    exit(1);
 }
 
 // convert uint32_t to its float
